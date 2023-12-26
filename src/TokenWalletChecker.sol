334cdbf30588a2acffffc3d9bab096512fd42d03
chinedu jeremiah <chinedujeremiah2002@gmail.com>
2023-12-26 13:50:11 +0100

    Added a check to verify the account address

--- src/TokenWalletChecker.sol
+++ src/TokenWalletChecker.sol
@@ -4,8 +4,18 @@ pragma solidity ^0.8.18;
 import {IERC20} from "../lib/forge-std/src/interfaces/IERC20.sol";
 
 contract TokenWalletChecker {
+    error InvalidAddress(address account);
+
     function getBalanceOfToken(address _owner, address _tokenContractAddress)external view returns(uint){
+        checkAddress(_owner);
+        checkAddress(_tokenContractAddress);
         IERC20 tokenContract = IERC20(_tokenContractAddress);
         return tokenContract.balanceOf(_owner);
+    }
+
+    function checkAddress(address account)internal pure {
+        if (account == address(0)){
+            revert InvalidAddress(account);
+        }
     }
 }
